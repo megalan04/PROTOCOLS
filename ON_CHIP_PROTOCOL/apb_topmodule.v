82c746fe2c7ed78ad524db01caf0fed3f0ba77d9
Megalan <megalan4444@gmail.com>
2026-03-12 16:03:11 +0530

    Refactor apb_topmodule with updated data widths

--- ON_CHIP_PROTOCOL/apb_topmodule.v
+++ ON_CHIP_PROTOCOL/apb_topmodule.v
@@ -1,15 +1,20 @@
+`include "master.v"
+`include "slave.v"
 module apb_topmodule (input pclk,
     input prreset,
-    input [8:0] pwdata, 
-    input [16:0]paddr,
+    input [31:0] pwdata, 
+    input [7:0]paddr,
     input transfer,
     input pwrite,
     output [31:0]prdata,
     output pslverr,
-  output [2:0]psel );  
-  wire penable;
+    output psel ,
+    output penable);  
   wire pready;
-  master_apb(.pclk(pclk),.prrset(prrset),.prdata(prdata),.pready(pready),.transfer(transfer),.pslverr(pslverr),.paddr(paddr),.psel(psel),.penable(penable),.pwrite(pwrite),.pwdata(pwdata));
-  slave_apb(.pclk(pclk),.prreset(prreset),.prdata(prdata),.pready(pready),.pslverr(pslverr),.paddr(paddr),.psel(psel),.penable(penable),.pwrite(pwrite),.pwdata(pwdata));
+  wire [7:0]addrreg;
+  wire [31:0]datareg;
+  wire en;
+  apb_protocol m1(.pclk(pclk),.prreset(prreset),.prdata(prdata),.pready(pready),.transfer(transfer),.pslverr(pslverr),.padrr(paddr),.psel(psel),.penable(penable),.pwrite(pwrite),.pwdata(pwdata),.addrreg(addrreg),.datareg(datareg),.en(en));
+  slave  s1(.pclk(pclk),.prreset(prreset),.prdata(prdata),.pready(pready),.pslverr(pslverr),.padrr(addrreg),.psel(psel),.penable(penable),.pwrite(en),.pwdata(datareg));
 
 endmodule
